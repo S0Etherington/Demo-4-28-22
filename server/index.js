64d15a1edee9736017f07957b1f377b5fc688e37
Summer Etherington <summersce23@gmail.com>
2022-04-28 12:44:13 -0600

    added rollbar logs

--- server/index.js
+++ server/index.js
@@ -3,6 +3,8 @@ const path = require('path')
 
 const app = express()
 
+app.use(express.json())
+
 // include and initialize the rollbar library with your access token
 var Rollbar = require('rollbar')
 var rollbar = new Rollbar({
@@ -14,13 +16,26 @@ var rollbar = new Rollbar({
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
 
-
-
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
 })
 
 const port = process.env.PORT || 4545
+
+let students = []
+
+app.post('/api/student', (req,res) => {
+    let {name} = req.body
+    name = name.trim()
+
+    students.push(name)
+
+    rollbar.log('Student added successfully', {author: 'Scott', type: 'manual entry'})
+
+    res.status(200).send(students)
+})
+
+app.use(rollbar.errorHandler())
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`)
