916e783e4d3ebee03fdd8ed93d9bd804545dc404
Summer Etherington <summersce23@gmail.com>
2022-04-28 14:10:02 -0600

    rollbar added

--- server/index.js
+++ server/index.js
@@ -18,6 +18,7 @@ rollbar.log('Hello world!')
 
 app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname, '../index.html'))
+    rollbar.info('File served')
 })
 
 const port = process.env.PORT || 4545
